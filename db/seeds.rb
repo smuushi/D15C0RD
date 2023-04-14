3937278d569f8accd7ec0c4c419c8c52f646d7a2
smuushi <mishih780@gmail.com>
2023-04-13 23:35:42 -0400

    fixed require

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-require "open_uri"
+require "open-uri"
 
 ActiveRecord::Base.transaction do 
 
