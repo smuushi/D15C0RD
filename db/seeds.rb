0fdeba8aeb18b8b09301bc6ed353af750a672300
smuushi <mishih780@gmail.com>
2023-04-13 23:32:33 -0400

    seed update

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,18 +32,23 @@ ActiveRecord::Base.transaction do
     User.create(username:"KaiterSeed", email:"kaiter@paired.com", password:"testing", about:"developer of spacebook! waifuhunter49", tag: "8903")
     User.create(username:"SaraSeed", email:"sara@paired.com", password:"testing", about:"zelp developer! nakama" ,tag: "1090")
     User.create(username:"ShanSeed", email:"shan@paired.com", password:"testing", about:"career caresser" ,tag: "1090")
-    User.create(username:"Michael Shih", email: "michael@paired.com", password:"testing", about:"Thanks for visiting my discord clone! Here are the links to my github and linkedin!", tag:"9876")
+    User.create(username:"Michael Shih", email: "michael@paired.com", password:"testing", about:"Thanks for visiting my discord clone! Visit my github!", tag:"9876")
     
 end
 
-User.all.each_with_index do |user, idx|
+    users = User.all 
 
-    user.avatar.attach(
-        io: ###file object here,
-        filename: ###some file here... .
-    )
+    users[2].avatar.attach(io: URI.open("https://d15c0rd-seeds.s3.amazonaws.com/garyseed.png"), filename: "garyseed.png")
+    users[3].avatar.attach(io: URI.open("https://d15c0rd-seeds.s3.amazonaws.com/kellyseed.png"), filename: "kellyseed.png")
+    users[4].avatar.attach(io: URI.open("https://d15c0rd-seeds.s3.amazonaws.com/samseed.png"), filename: "samseed.png")
+    users[5].avatar.attach(io: URI.open("https://d15c0rd-seeds.s3.amazonaws.com/milnerseed.png"), filename: "milnerseed.png")
+    users[6].avatar.attach(io: URI.open("https://d15c0rd-seeds.s3.amazonaws.com/timseed.png", filename: "timseed.png"))
+    users[7].avatar.attach(io: URI.open("https://d15c0rd-seeds.s3.amazonaws.com/kaiterseed.png"), filename: "kaiterseed.png")
+    users[8].avatar.attach(io: URI.open("https://d15c0rd-seeds.s3.amazonaws.com/saraseed.png"), filename: "saraseed.png")
+    users[9].avatar.attach(io: URI.open("https://d15c0rd-seeds.s3.amazonaws.com/shanseed.png"), filename: "shanseed.png")
+    users[10].avatar.attach(io: URI.open("https://cdn.discordapp.com/attachments/1068364632269525106/1096275984271024128/IMG_5280.jpg"), filename: "michaelseed")
 
-end
+
 
 
 
@@ -56,33 +61,19 @@ ActiveRecord::Base.transaction do
     # User.create(username:"testing", email:"papaya@paired.com", password:"testing", tag: "1090")
     Server.create(name:"Demo Room 1", owner_id: 1)
 
-
     Server.create(name:"Demo Room 2", owner_id: 1)
-
 
     Server.create(name:"Michael was here", owner_id: 1)
 
-
-
     Server.create(name:"Fruit", owner_id: 2)
-
 
     Server.create(name:"Vegatables", owner_id: 2)
 
-
-
     Server.create(name:"Reminders", owner_id: 3)
-
 
     Server.create(name:"train is late", owner_id: 3)
 
-
-
     Server.create(name:"sbotify", owner_id:4)
-
-
-
-    
 
 end
 
